e8d6f90a7f4c18890c5efd7e5bb6245aba7a4bb8
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-16 15:36:41 +0100

    job 07

--- Jour01/job07/index.php
+++ Jour01/job07/index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 
     <head>
-        <title>Runtrack3-Job6</title>
+        <title>Runtrack3-Job7</title>
         <link rel="stylesheet" href="style.css">
         <script src="script.js"></script>
     </head>
