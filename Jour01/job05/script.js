477145fffa91241a8ed1e79e5354df74da7c96fb
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-16 14:18:24 +0100

    job 05

--- Jour01/job05/script.js
+++ Jour01/job05/script.js
@@ -1,15 +1,11 @@
-let année = prompt("Choisissez une année");
 
-function bisextile(année){
-	//conditions  : https://fr.wikipedia.org/wiki/Ann%C3%A9e_bissextile
-	if ((année % 4 === 0 && année % 100 > 0) || (année % 400 === 0)) {
-		return true;
-	} else {
-		return false;
-	}
+function afficherjourssemaines(){
+	const jourssemaines=["lundi","mardi","mercredis","jeudi","vendredis","samedis","dimanche"];
+	for (const jours of jourssemaines) {
+		console.log(jours);
+	  }
 }
-if (bisextile(année)=true){
-	alert(année + " est bissextile");
-}else if(bisextile(année)=false){
-	alert(année + " n'est pas bissextile");
-}
+
+afficherjourssemaines();
+
+
