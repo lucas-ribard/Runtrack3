9facd4208f77613d2301f3c0fb591829a37749c6
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-16 13:54:24 +0100

    job4

--- Jour01/job04/script.js
+++ Jour01/job04/script.js
@@ -1,8 +1,15 @@
-let year = prompt("Choisissez une année");
+let année = prompt("Choisissez une année");
 
-//condition trouvé sur : https://fr.wikipedia.org/wiki/Ann%C3%A9e_bissextile
-if ((year % 4 === 0 && year % 100 > 0) || (year % 400 === 0)) {
-	alert(year + " est bissextile");
-} else {
-	alert(year + " n'est pas bissextile");
+function bisextile(année){
+	//conditions  : https://fr.wikipedia.org/wiki/Ann%C3%A9e_bissextile
+	if ((année % 4 === 0 && année % 100 > 0) || (année % 400 === 0)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+if (bisextile(année)=true){
+	alert(année + " est bissextile");
+}else if(bisextile(année)=false){
+	alert(année + " n'est pas bissextile");
 }
