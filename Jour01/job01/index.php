500d68d2365dc96c03ca4a8bf77fcb9728db3c40
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-16 12:24:21 +0100

    Job -1 et 2

---
+++ Jour01/job01/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+
+    <head>
+        <title>Runtrack3-Job1</title>
+        <link rel="stylesheet" href="style.css">
+        <script>alert("Hello Javascript!");</script>
+    </head>
+
+    <body>
+    
+    </body>
+
+</html>
