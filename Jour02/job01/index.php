492ceac18db1faf9194909b33d0d3dd76c00b36b
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-17 12:49:53 +0100

    J2 Job 01

---
+++ Jour02/job01/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Runtrack3-Job1</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <article id="citation" >La vie a beaucoup plus d’imagination que nous</article>
+    <button id="button">citation</button>
+    
+</body>
+
+</html>
---
+++ Jour02/job01/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded",function citation(){
+	const Citation = document.getElementById("citation");
+	const button = document.getElementById("button");
+	button.addEventListener("click", ()=>{console.log(Citation);});
+});
