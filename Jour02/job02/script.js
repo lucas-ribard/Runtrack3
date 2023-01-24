c181f7006646b5b82429f7726cfaeb4a336fc381
lucas-ribard <lucas.ribard@laplateforme.io>
2023-01-24 10:52:03 +0100

    J2 Job 02

--- Jour02/job02/script.js
+++ Jour02/job02/script.js
@@ -1,17 +1,17 @@
 function showhide(){
 
 	const test =document.querySelector("article");
+
 	if(!test){
+
 		var art = document.createElement("article");
 		var citation = document.createTextNode("L'important n'est pas la chute, mais l'atterrissage.");
 		art.appendChild(citation);
 		document.body.appendChild(art);
-  }
-  else
-  {
+  	}
+  else{
   	test.remove();
-  }
-	};
+  }};
 
 
 
